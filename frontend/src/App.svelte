8563828b4820636475e602f29938bfcb474fd29f
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-10-18 20:48:41 +0200

    Show latest info

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import Value from "./lib/Value.svelte";
 
   let data: ClimatePoint[] = [];
+  $: last = data[data.length - 1];
 
   onMount(async () => {
     const req = await fetch("http://pc.lego:5000/api/data");
@@ -11,4 +13,36 @@
   });
 </script>
 
-<h1 class="text-3xl font-semibold">Hello World</h1>
+<div class="container mx-auto my-2">
+  <h1 class="text-3xl font-semibold">Hello World</h1>
+
+  {#if last}
+    <div class="flex gap-2 justify-between">
+      <Value
+        label="temperature"
+        symbol="°C"
+        value={last.temperature}
+        min={-10}
+        max={50}
+      />
+
+      <Value
+        label="Humidity"
+        symbol="%"
+        value={last.humidity}
+        min={0}
+        max={100}
+      />
+
+      <Value
+        label="Pressure"
+        symbol="hPa"
+        value={last.pressure}
+        min={0}
+        max={100}
+      />
+    </div>
+  {/if}
+
+  <pre>{JSON.stringify(data, null, 2)}</pre>
+</div>
---
+++ frontend/src/lib/Value.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let value: number;
+  export let min: number;
+  export let max: number;
+  export let label: string;
+  export let symbol: string;
+</script>
+
+<div class="w-full text-center">
+  <p class="font-semibold text-xl">{value}{symbol}</p>
+  <p class="font-light text-sm">{label}</p>
+</div>
