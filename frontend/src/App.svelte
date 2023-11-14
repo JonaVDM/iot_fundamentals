f56c2eb10315a92156ad5b77440e0d9c460e108b
Jonathan <32264711+JonaVDM@users.noreply.github.com>
2023-11-14 15:23:02 +0100

    Added graph

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,21 +1,22 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import Value from "./lib/Value.svelte";
+  import Graph from "./lib/Graph.svelte";
 
   let data: ClimatePoint[] = [];
-  $: last = data[data.length - 1];
+  $: last = data[0];
+  $: labels = data.map((d) => new Date(d.time).toLocaleTimeString()); // const labels =
+  $: date = new Date(last?.time);
 
   onMount(async () => {
     const req = await fetch("http://pc.lego:5000/api/data");
     const body = await req.json();
 
-    data = body.data;
+    data = body.data.reverse();
   });
 </script>
 
 <div class="container mx-auto my-2">
-  <h1 class="text-3xl font-semibold">Hello World</h1>
-
   {#if last}
     <div class="flex gap-2 justify-between">
       <Value
@@ -42,7 +43,15 @@
         max={100}
       />
     </div>
+
+    <p class="text-sm font-extralight italic">
+      Last measurement at {date.toLocaleString()}
+    </p>
   {/if}
 
-  <pre>{JSON.stringify(data, null, 2)}</pre>
+  {#key data}
+    <Graph data={data.map((d) => d.temperature)} {labels} label="Temperature" />
+    <Graph data={data.map((d) => d.humidity)} {labels} label="Humidity" />
+    <Graph data={data.map((d) => d.pressure)} {labels} label="Pressure" />
+  {/key}
 </div>
---
+++ frontend/src/lib/Graph.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { Chart } from "chart.js/auto";
+
+  export let labels: string[];
+  export let data: number[];
+  export let label: string;
+
+  function chartAction(container: HTMLCanvasElement) {
+    const ctx = container.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels,
+        datasets: [
+          {
+            data,
+            label,
+          },
+        ],
+      },
+    });
+  }
+</script>
+
+<canvas height="60px" use:chartAction />
